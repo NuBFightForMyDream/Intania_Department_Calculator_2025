bd15a3da9521ef04d05e131f742db9c789b97225
chatrphol.nine <89005054+NuBFightForCP51Again@users.noreply.github.com>
2025-06-15 11:52:57 +0000

    Update info

--- main_calculator.py
+++ main_calculator.py
@@ -1,16 +1,19 @@
 # import libraries (streamlit & pandas (for dataFrame) )
 import streamlit as st
 import pandas as pd # for DataFrame
+import numpy as np # for calculting mathematics function
 
 # Step 1 : set page config (layout message)
 st.set_page_config(page_title="Intania Department Calculator 2025", layout="wide")
 st.title("Intania Department Calculator 🧮")
 st.markdown("Welcome to my Intania Department Calculator , This program can help you to calculate possibility to reach your dream department. Hop you enjoy")
 
+st.image("/workspaces/Intania_Department_Calculator/Multiplier Intania Department.jpg" , caption = "Gei")
+
 # 💡 put selection box outside (Study Group & Dream Department)
 
-# Step 2 : input info
-# create selectbox with list of departments (Lists)
+# Step 2 : input basic info
+# 2.1 create selectbox with list of departments (Lists)
 dream_department = st.selectbox(
     
     "อยากเป็น #2 อะไรกันน",
@@ -24,10 +27,10 @@ dream_department = st.selectbox(
 
 start_course = st.selectbox("Study Group", ["ENG DRAWING First", "COMP PROG First"])
 
-# create dict for storing key;value of grade
+# 2.2 create dict for storing key;value of grade
 grades = {'A' : 4.00 , 'B+' : 3.50 , 'B' : 3.00 , 'C+' : 2.50 , 'C' : 2.00 , 'D+' : 1.50 , 'D' : 1.00 , 'F' : 0.00 , 'W' : 0.00}
 
-# create form for input grades
+# 2.3 create form for input grades -> don't forget st.form_submit_button (must)
 with st.form("Intania Department Calculator"):
     
     if start_course == "ENG DRAWING First":
@@ -68,8 +71,16 @@ with st.form("Intania Department Calculator"):
         labphy2 = st.selectbox("⚛️2304184 General Physics Laboratory II Grade" , list(grades.keys()))
         eng2 = st.selectbox("🧑‍🔬5500112 Experimental Engineering II Grade" , list(grades.keys()))
 
-# create list for storing all of users's grades
-your_grade = [compprog , explore , material , drawing , cal1 , cal2 , phy1 , phy2 , genchem , labchem , labphy1 , labphy2 , eng1 , eng2]
+    # 2.4 create submit button for calculating points
+    submitted = st.form_submit_button("Submit")
+
+
+# 2.5 create list for storing all of users's grades
+your_grade_letter = [compprog , explore , material , drawing , cal1 , cal2 , phy1 , phy2 , genchem , labchem , labphy1 , labphy2 , eng1 , eng2]
+
+your_grade_value = [grades[compprog] , grades[explore] , grades[material], grades[drawing] , grades[cal1] , grades[cal2] ,
+                    grades[phy1] , grades[phy2] , grades[genchem] , grades[labchem] , grades[labphy1] , grades[labphy2] , grades[eng1] , grades[eng2] ]
+
 
 # Step 3 : create weights (dict) for each department [key = dep_name , value = weight]
 weights = {
@@ -86,25 +97,103 @@ weights = {
             "Automotive Engineering (AE) 🚗"     : [3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 1 , 1 , 1 , 3 , 3]
 }
 
-# define var.
-cp_total = 0 ; ee_total = 0 ; me_total = 0 ; ie_total = 0 ; che_total = 0 ; pe_total = 0 ; 
-ae_total = 0 ; sv_total = 0 ; mt_total = 0 ; ce_total = 0 ; env_total = 0
+# Step 4 : calculate score for each department
+score_each_dept = {} # dict ( for key (dept_name) :value (score) )
 
-score_each_dep = []
+# outer loop : extract key (each_dept) and value (weight_list) from weights dict
+for (each_dept , weight_list) in weights.items() : 
+    # create sum_score var. (reset for each dept)
+    sum_score = 0
+    # inner loop : calculate score for each department
+    for pos in range(len(weight_list)):
 
-for each_dep in weights : 
-    cp_total += weights[]
-    
+        # Note : we can use (pos , weight_each_subj) in enumerate(weight_list) to get index and weight (similar to zip() but not all)
 
+        # calculate score for each subject
+        sum_score += your_grade_value[pos] * weight_list[pos]
+            # enumerate form : sum_score += your_grade_value[pos] * weight_each_subj
 
+    # store the score in score_each_dep dict
+    score_each_dept[each_dept] = sum_score
 
+# Step 5 : Calculate possibilities for each department
+    # Logic : based on 6-year data backward , we'll calculate 2 ways
+        # 1st way : calculate by average of min_score
+        # 2nd way : calculate by weighted min_score (more accurate)
 
+    # 5.1 : create dict for storing minscore of each department
+    minscore_dept = {
+        "Computer Engineering (CP) 💻"           : [125 , 136 , 140 , 141.5 , 153 , 115] , 
+            "Electrical Engineering (EE) ⚡"     : [107 , 118.5 , 121 , 110 , 124 , 87.5] , 
+            "Industrial Engineering (IE) 🏭"     : [98 , 107 , 115.5 ,112 , 116.5 , 98.5] , 
+            "Mechanical Engineering (ME) ⚙️"     : [100 , 99 , 102.5 , 88 , 93.5 , 79.5] ,
+            "Civil Engineering (CE) ⛑️"          : [78.5 , 87.5 , 90 , 74.5 , 76 , 7.5] ,
+            "Metallurgical Engineering (MT) 🦾"  : [35 , 58.5 , 35 , 64 , 49.5 , 39] , 
+            "Chemical Engineering (CHE) 🧪"      : [77.5 , 63 , 41.5 , 34 , 66.5 , 57.5] ,
+            "Petroleum Engineering (PE) ⚗️"      : [94.5 , 90 , 87.5 , 74 , 97.5, 74.5] , 
+            "Environmental Engineering (ENV) 🌲" : [82 , 76.5 , 90 , 80.5 , 81.5 , 77.5] , 
+            "Survey Engineering (SV) 🔎"         : [69 , 66.5 , 84.5 , 101 , 35.5 , 55.5] , 
+            "Automotive Engineering (AE) 🚗"     : [76.5 , 88 , 87.5 , 71.5 , 76 , 70.5]
+    }
 
+# 5.3 : create dict for storing weighted minscore of each department
+weighted_minscore = {
+        "Computer Engineering (CP) 💻"       : (0.16 * 125  + 0.16 * 136   + 0.16 * 140   + 0.16 * 141.5 + 0.16 * 153   + 0.2 * 115) , 
+        "Electrical Engineering (EE) ⚡"     : (0.16 * 107  + 0.16 * 118.5 + 0.16 * 121   + 0.16 * 110   + 0.16 * 124   + 0.2 * 87.5) , 
+        "Industrial Engineering (IE) 🏭"     : (0.16 * 98   + 0.16 * 107   + 0.16 * 115.5 + 0.16 * 112   + 0.16 * 116.5 + 0.2 * 98.5) , 
+        "Mechanical Engineering (ME) ⚙️"     : (0.16 * 100  + 0.16 * 99    + 0.16 * 102.5 + 0.16 * 88    + 0.16 * 93.5  + 0.2 * 79.5) ,
+        "Civil Engineering (CE) ⛑️"          : (0.19 * 78.5 + 0.19 * 87.5  + 0.19 * 90    + 0.19 * 74.5  + 0.19 * 76    + 0.05 * 7.5) ,
+        "Metallurgical Engineering (MT) 🦾"  : (0.16 * 35   + 0.16 * 58.5  + 0.16 * 35    + 0.16 * 64    + 0.16 * 49.5  + 0.2 * 39) , 
+        "Chemical Engineering (CHE) 🧪"      : (0.16 * 77.5 + 0.16 * 63    + 0.16 * 41.5  + 0.16 * 34    + 0.16 * 66.5  + 0.2 * 57.5) ,
+        "Petroleum Engineering (PE) ⚗️"      : (0.16 * 94.5 + 0.16 * 90    + 0.16 * 87.5  + 0.16 * 74    + 0.16 * 97.5  + 0.2 * 74.5) , 
+        "Environmental Engineering (ENV) 🌲" : (0.16 * 82   + 0.16 * 76.5  + 0.16 * 90    + 0.16 * 80.5  + 0.16 * 81.5  + 0.2 * 77.5) , 
+        "Survey Engineering (SV) 🔎"         : (0.16 * 69   + 0.16 * 66.5  + 0.16 * 84.5  + 0.16 * 101   + 0.16 * 35.5  + 0.2 * 55.5) , 
+        "Automotive Engineering (AE) 🚗"     : (0.16 * 76.5 + 0.16 * 88    + 0.16 * 87.5  + 0.16 * 71.5  + 0.16 * 76    + 0.2 * 70.5)
+    }
+
+# Step 6 : calculate possibility for each department (2 ways) 
+average_min_possibility = {
+        "Computer Engineering (CP) 💻"       : (0.16 * 125  + 0.16 * 136   + 0.16 * 140   + 0.16 * 141.5 + 0.16 * 153   + 0.2 * 115) , 
+        "Electrical Engineering (EE) ⚡"     : (0.16 * 107  + 0.16 * 118.5 + 0.16 * 121   + 0.16 * 110   + 0.16 * 124   + 0.2 * 87.5) , 
+        "Industrial Engineering (IE) 🏭"     : (0.16 * 98   + 0.16 * 107   + 0.16 * 115.5 + 0.16 * 112   + 0.16 * 116.5 + 0.2 * 98.5) , 
+        "Mechanical Engineering (ME) ⚙️"     : (0.16 * 100  + 0.16 * 99    + 0.16 * 102.5 + 0.16 * 88    + 0.16 * 93.5  + 0.2 * 79.5) ,
+        "Civil Engineering (CE) ⛑️"          : (0.19 * 78.5 + 0.19 * 87.5  + 0.19 * 90    + 0.19 * 74.5  + 0.19 * 76    + 0.05 * 7.5) ,
+        "Metallurgical Engineering (MT) 🦾"  : (0.16 * 35   + 0.16 * 58.5  + 0.16 * 35    + 0.16 * 64    + 0.16 * 49.5  + 0.2 * 39) , 
+        "Chemical Engineering (CHE) 🧪"      : (0.16 * 77.5 + 0.16 * 63    + 0.16 * 41.5  + 0.16 * 34    + 0.16 * 66.5  + 0.2 * 57.5) ,
+        "Petroleum Engineering (PE) ⚗️"      : (0.16 * 94.5 + 0.16 * 90    + 0.16 * 87.5  + 0.16 * 74    + 0.16 * 97.5  + 0.2 * 74.5) , 
+        "Environmental Engineering (ENV) 🌲" : (0.16 * 82   + 0.16 * 76.5  + 0.16 * 90    + 0.16 * 80.5  + 0.16 * 81.5  + 0.2 * 77.5) , 
+        "Survey Engineering (SV) 🔎"         : (0.16 * 69   + 0.16 * 66.5  + 0.16 * 84.5  + 0.16 * 101   + 0.16 * 35.5  + 0.2 * 55.5) , 
+        "Automotive Engineering (AE) 🚗" 
+}
 
 
 # ประมวลผลหลัง submit
-# if submitted:
-    #df = pd.DataFrame([{"ประเทศ": country, "ปี": year, "GDP": gdp}])
-    #st.write("ข้อมูลที่กรอก:")
-    #st.dataframe(df)
-    #st.success(f"GDP ของ {country} ในปี {year} คือ {gdp:,}")
+if submitted:
+
+
+
+
+
+
+
+
+
+
+
+
+    st.subheader("📊 คะแนนรวมของแต่ละภาค:")
+    for dep, score in score_each_dep.items():
+        st.write(f"**{dep}** : {score:.2f} คะแนน")
+    
+    # เพิ่มการเน้นภาคที่ฝันไว้
+    if dream_department in score_each_dep:
+        dream_score = score_each_dep[dream_department]
+        st.markdown(f"🎯 คะแนนของภาคในฝัน ({dream_department}) ของคุณคือ: **{dream_score:.2f}**")
+
+        if dream_score >= 50:
+            st.success("โอกาสสูงมาก! ✅")
+        elif dream_score >= 40:
+            st.warning("โอกาสกลาง ๆ พอลุ้นได้ 🟡")
+        else:
+            st.error("ต้องพยายามเพิ่มอีกหน่อย ❌")
+
